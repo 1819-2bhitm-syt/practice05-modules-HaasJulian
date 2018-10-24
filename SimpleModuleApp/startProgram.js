e89dc3ec7774b0eb4fe620ccb45e8190318335ef
Haas Julian <43719974+HaasJulian@users.noreply.github.com>
2018-10-24 15:04:02 +0200

    Delete startProgram.js

--- SimpleModuleApp/startProgram.js
+++
@@ -1,9 +0,0 @@
-let waitTime3=10;
-let waitTime2=20;
-let waitTime1=15;
-
-let startProgramm = function(program, waitTime, programName){
-    console.log("***Program "+programeName+" will start in" + waitTime);
-    require("./app");
-
-};
