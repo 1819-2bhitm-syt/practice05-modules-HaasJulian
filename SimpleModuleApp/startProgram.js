a37e878bd11ae05a50f39867ad412d12be1a0ba0
Haas Julian <43719974+HaasJulian@users.noreply.github.com>
2018-10-24 14:18:58 +0200

    Simple Modules Uebung 05

---
+++ SimpleModuleApp/startProgram.js
@@ -0,0 +1,9 @@
+let waitTime3=10;
+let waitTime2=20;
+let waitTime1=15;
+
+let startProgramm = function(program, waitTime, programName){
+    console.log("***Program "+programeName+" will start in" + waitTime);
+    require("./app");
+
+};
